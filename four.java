d51251e68739c2d75a1068f33605869ff284208e
yashwanth7337 <yash.yashwanth123@gmail.com>
2023-06-28 13:33:49 +0530

    hi

---
+++ four.java
@@ -0,0 +1,19 @@
+public class four {
+   int a=10;//instance variable
+   static int number=100;//static variable
+    
+   public static void main(String[] args) {
+   four f= new four();
+   System.out.println(number);
+   System.out.println(f.a);
+   int b=200;//local variable
+   System.out.println(b);
+   
+
+    }
+    
+
+    
+    
+   }
+    
